2ff5483d060110b4fd15d84e8c630a560c1f0b9b
eeoooue <pdmoorhouse@gmail.com>
2025-08-17 14:09:48 +0100

    drafted OthelloGameState implementation

--- GameAPI/LibOthello/OthelloGameState.cs
+++ GameAPI/LibOthello/OthelloGameState.cs
@@ -8,5 +8,40 @@ namespace LibOthello
 {
     public class OthelloGameState
     {
+        public int[,] Board;
+        public int TurnPlayer;
+
+        public OthelloGameState(OthelloGame game)
+        {
+            Board = CopyGameBoard(game.Board);
+            TurnPlayer = ParseTurnPlayer(game.TurnPlayer);
+        }
+
+        private int[,] CopyGameBoard(int[,] board)
+        {
+            int[,] result = new int[8, 8];
+
+            for(int i=0; i<8; i++)
+            {
+                for(int j=0; j<8; j++)
+                {
+                    result[i, j] = board[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        private int ParseTurnPlayer(OthelloPiece piece)
+        {
+            if (piece == OthelloPiece.Black)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
     }
 }
