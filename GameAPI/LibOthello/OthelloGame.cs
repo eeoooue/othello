4bb6d29e9d1b3ccdd74850a4a5e62f4dbcb889b2
eeoooue <pdmoorhouse@gmail.com>
2025-08-17 12:25:39 +0100

    scaffolded methods for OthelloGame class

--- GameAPI/LibOthello/OthelloGame.cs
+++ GameAPI/LibOthello/OthelloGame.cs
@@ -6,8 +6,12 @@
         public const int BoardWidth = 8;
         public int[,] Board { get { return _board; } }
 
+        public OthelloPiece TurnPlayer = OthelloPiece.Black;
+
         private List<OthelloMove> _moves = new List<OthelloMove>();
         private int[,] _board = new int[BoardWidth, BoardWidth];
+
+        private CaptureAnalyst _captureAnalyst = new CaptureAnalyst();
 
         public OthelloGame()
         {
@@ -28,6 +32,47 @@
             _board[3, 4] = 1;
             _board[4, 3] = -1;
             _board[3, 4] = 1;
+        }
+
+        private void RefreshListOfAvailableMoves()
+        {
+
+        }
+
+        public void AttemptMove(OthelloMove move)
+        {
+            if (MoveIsByTurnPlayer(move) && MoveIsValid(move))
+            {
+                PerformMove(move);
+                RefreshListOfAvailableMoves();
+            }
+        }
+
+        private void PerformMove(OthelloMove move)
+        {
+            return;
+        }
+
+        private bool MoveIsValid(OthelloMove move)
+        {
+            return false;
+        }
+
+        private bool MoveIsByTurnPlayer(OthelloMove move)
+        {
+            return false;
+        }
+
+        private int ParsePieceColour(OthelloPiece piece)
+        {
+            if (piece == OthelloPiece.Black)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
