abd4cd49a637ce54fea9872f24b340a1d3a66c2d
eeoooue <pdmoorhouse@gmail.com>
2025-08-17 12:01:45 +0100

    drafted an OthelloGame class

--- GameAPI/LibOthello/OthelloGame.cs
+++ GameAPI/LibOthello/OthelloGame.cs
@@ -2,6 +2,32 @@
 {
     public class OthelloGame
     {
+        public List<OthelloMove> Moves { get { return _moves; } }
+        public const int BoardWidth = 8;
+        public int[,] Board { get { return _board; } }
 
+        private List<OthelloMove> _moves = new List<OthelloMove>();
+        private int[,] _board = new int[BoardWidth, BoardWidth];
+
+        public OthelloGame()
+        {
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for(int i=0; i< BoardWidth; i++)
+            {
+                for(int j=0; j<BoardWidth; j++)
+                {
+                    _board[i,j] = 0;
+                }
+            }
+
+            _board[4, 3] = -1;
+            _board[3, 4] = 1;
+            _board[4, 3] = -1;
+            _board[3, 4] = 1;
+        }
     }
 }
