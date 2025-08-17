aff8f911f5849d64841109982b7d058de91076a4
eeoooue <pdmoorhouse@gmail.com>
2025-08-17 13:52:21 +0100

    created a DevConsole project for testing functionality

---
+++ GameAPI/DevConsole/GamePresenter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevConsole
+{
+    internal class GamePresenter
+    {
+        public void PresentBoard(int[,] board)
+        {
+            for(int i=0; i<8; i++)
+            {
+                PresentRow(board, i);
+            }
+        }
+
+        private void PresentRow(int[,] board, int i)
+        {
+            StringBuilder sb = new StringBuilder();
+            for(int j=0; j<8; j++)
+            {
+                string x = ConvertCell(board[i,j]);
+                sb.Append(x);
+            }
+            string built = sb.ToString();
+            Console.WriteLine(built);
+        }
+
+        private string ConvertCell(int value)
+        {
+            if (value == 1)
+            {
+                return "X";
+            }
+            else if (value == -1)
+            {
+                return "O";
+            }
+            return "_";
+        }
+    }
+}
--- GameAPI/DevConsole/Program.cs
+++ GameAPI/DevConsole/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace DevConsole
+﻿using LibOthello;
+
+namespace DevConsole
 {
     internal class Program
     {
+        private static OthelloGame Game = new OthelloGame();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GamePresenter presenter = new GamePresenter();
+
+            while (true)
+            {
+                Console.Clear();
+                presenter.PresentBoard(Game.Board);
+                GetPlayerMove();
+            }
+        }
+
+        static void GetPlayerMove()
+        {
+            Console.WriteLine("Make a move, e.g. 0 0");
+            string? input = Console.ReadLine();
+            if (input is string value)
+            {
+                try
+                {
+                    AttemptMove(value);
+                }
+                catch
+                {
+                    return;
+                }
+            }
+        }
+
+        static void AttemptMove(string moveString)
+        {
+            if (moveString == "pass")
+            {
+                Game.AttemptPass();
+                return;
+            }
+
+            string[] arr = moveString.Split(" ");
+
+            int i = int.Parse(arr[0]);
+            int j = int.Parse(arr[1]);
+            OthelloPiece player = Game.TurnPlayer;
+            OthelloMove move = new OthelloMove(player, i, j);
+            Game.AttemptMove(move);
         }
     }
 }
