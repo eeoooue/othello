8bc262678e69d1c46e090ca48f3fe32653be5078
eeoooue <pdmoorhouse@gmail.com>
2025-08-22 16:15:27 +0100

    Create MoveDataObject.cs

---
+++ GameAPI/OthelloGameAPI/DataObjects/MoveDataObject.cs
@@ -0,0 +1,9 @@
+﻿namespace OthelloGameAPI.DataObjects
+{
+    public class MoveDataObject
+    {
+        public int Player { get; set; }
+        public int I { get; set; }
+        public int J { get; set; }
+    }
+}
