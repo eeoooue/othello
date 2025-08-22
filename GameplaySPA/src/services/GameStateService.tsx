f4a7df57dede8efd8f9591560a8e04dedf897f8f
eeoooue <pdmoorhouse@gmail.com>
2025-08-22 20:53:00 +0100

    updated GameStateService to hide move options for white

--- GameplaySPA/src/services/GameStateService.tsx
+++ GameplaySPA/src/services/GameStateService.tsx
@@ -4,6 +4,16 @@ import { getGameInfoFromApi, type GameInfo } from "./GameInfoService";
 
 export async function fetchGameState(): Promise<{ tiles: Piece[]; gameInfo: GameInfo }> {
   const [tiles, gameInfo] = await Promise.all([loadCurrentBoard(), getGameInfoFromApi()]);
+
+  if (gameInfo.TurnPlayer == -1){
+    const n: number = tiles.length;
+    for(let i=0; i<n; i++){
+      if (tiles[i] == "option"){
+        tiles[i] = "empty";
+      }
+    }
+  }
+
   return { tiles, gameInfo };
 }
 
