2847e2e0c16a9ab016672a3957677cd05d9559a6
eeoooue <pdmoorhouse@gmail.com>
2025-08-22 16:32:03 +0100

    created a GameInfoDataObject

---
+++ GameAPI/OthelloGameAPI/DataObjects/GameInfoDataObject.cs
@@ -0,0 +1,15 @@
+﻿namespace OthelloGameAPI.DataObjects
+{
+    public class GameInfoDataObject
+    {
+        public int TurnPlayer { get; set; }
+
+        public int ScoreBlack { get; set; }
+
+        public int ScoreWhite { get; set; }
+
+        public bool PlayerMustPass { get; set; }
+
+        public bool GameOver { get; set; }
+    }
+}
