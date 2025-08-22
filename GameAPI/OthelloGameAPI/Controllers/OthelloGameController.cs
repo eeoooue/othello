a4744dbf0775b1a1ea3794e4feb6ea5f0201af6f
eeoooue <pdmoorhouse@gmail.com>
2025-08-22 16:32:20 +0100

    moved some OthelloGameController responsibilities into OthelloGameService

--- GameAPI/OthelloGameAPI/Controllers/OthelloGameController.cs
+++ GameAPI/OthelloGameAPI/Controllers/OthelloGameController.cs
@@ -11,8 +11,6 @@ namespace OthelloGameAPI.Controllers
     public class OthelloGameController : ControllerBase
     {
         private OthelloGameService Service;
-        private OthelloGame Game { get { return Service.Game; } }
-        private OthelloGameState GameState { get { return Game.GetGameState(); } }
 
         public OthelloGameController(OthelloGameService service)
         {
@@ -28,51 +26,22 @@ namespace OthelloGameAPI.Controllers
         [HttpGet("Board")]
         public IActionResult GetBoard()
         {
-            int[,] board = GameState.Board;
-            int[][] matrix = CreateJaggedMatrix(board);
+            int[][] matrix = Service.GetGameBoard();
             return Ok(matrix);
-        }
-
-        private static int[][] CreateJaggedMatrix(int[,] grid)
-        {
-            int rows = grid.GetLength(0), cols = grid.GetLength(1);
-            var jagged = new int[rows][];
-            for (int i = 0; i < rows; i++)
-            {
-                jagged[i] = new int[cols];
-                for (int j = 0; j < cols; j++) jagged[i][j] = grid[i, j];
-            }
-            return jagged;
         }
 
         [HttpGet("TurnPlayer")]
         public IActionResult GetTurnPlayer()
         {
-            int turnPlayer = GameState.TurnPlayer;
+            int turnPlayer = Service.GetTurnPlayer();
             return Ok(turnPlayer);
         }
 
         [HttpPost("Move")]
-        public IActionResult SubmitMove([FromBody] MoveDataObject dataObject)
+        public IActionResult SubmitMove([FromBody] MoveDataObject move)
         {
-            OthelloPiece player = ParseTurnPlayer(dataObject.Player);
-            int i = dataObject.I;
-            int j = dataObject.J;
-            OthelloMove move = new OthelloMove(player, i, j);
-            Game.AttemptMove(move);
+            Service.AttemptMove(move);
             return Ok();
-        }
-
-        private OthelloPiece ParseTurnPlayer(int player)
-        {
-            if (player == 1)
-            {
-                return OthelloPiece.Black;
-            }
-            else
-            {
-                return OthelloPiece.White;
-            }
         }
 
         [HttpPost("NewGame")]
