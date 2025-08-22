80883729ac33f1e252550347d954399acae5f05b
eeoooue <pdmoorhouse@gmail.com>
2025-08-22 21:17:27 +0100

    added async call for opponent to make a move

--- GameAPI/OthelloGameAPI/Controllers/OthelloGameController.cs
+++ GameAPI/OthelloGameAPI/Controllers/OthelloGameController.cs
@@ -19,6 +19,25 @@ namespace OthelloGameAPI.Controllers
             Opponent = opponent;
         }
 
+        private async Task TriggerOpponentResponseAttempt()
+        {
+            if (Service.GetTurnPlayer() == 1 || Service.Game.GameOver)
+            {
+                return;
+            }
+
+            OthelloMove? possibleMove = await Opponent.GetOpponentMove(Service.Game);
+            if (possibleMove is OthelloMove move)
+            {
+                Service.Game.AttemptMove(move);
+            }
+            else
+            {
+                Service.Game.AttemptPass();
+            }
+        }
+
+
         [HttpGet("GameInfo")]
         public IActionResult GetGameInfo()
         {
@@ -44,6 +63,7 @@ namespace OthelloGameAPI.Controllers
         public IActionResult SubmitMove([FromBody] MoveDataObject move)
         {
             Service.AttemptMove(move);
+            _ = TriggerOpponentResponseAttempt();
             return Ok();
         }
 
@@ -51,6 +71,7 @@ namespace OthelloGameAPI.Controllers
         public IActionResult SubmitPass([FromQuery] int player)
         {
             Service.AttemptPass(player);
+            _ = TriggerOpponentResponseAttempt();
             return Ok();
         }
 
