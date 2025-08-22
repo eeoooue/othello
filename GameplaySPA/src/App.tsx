49dccb24dd16902b42b5ed7c3b9a595f2a20d003
eeoooue <pdmoorhouse@gmail.com>
2025-08-22 18:31:09 +0100

    simplified App.tsx using GameStateService and handing down an additional onReset handler

--- GameplaySPA/src/App.tsx
+++ GameplaySPA/src/App.tsx
@@ -6,42 +6,41 @@ import NavBar from './components/NavBar';
 import GameInterface from './components/GameInterface';
 import { createEmptyBoard, loadCurrentBoard } from './services/BoardService';
 import type { Piece } from './services/BoardService';
-import { submitMove } from "./services/MoveService";
+import { submitMove, startNewGame } from "./services/MoveService";
 import { GameInfo, getDummyGameInfo, getGameInfoFromApi } from './services/GameInfoService';
+
+import { fetchGameState } from './services/GameStateService';
+
 
 function App() {
   const [tiles, setTiles] = useState<Piece[]>(() => createEmptyBoard());
   const [gameInfo, setGameInfo] = useState<GameInfo>(() => getDummyGameInfo());
 
-  useEffect(() => {
-    loadCurrentBoard().then(setTiles).catch(console.error);
-  }, []);
+  const refreshGameState = async () => {
+    const { tiles, gameInfo } = await fetchGameState();
+    setTiles(tiles);
+    setGameInfo(gameInfo);
+  };
 
-  useEffect(() => {
-    getGameInfoFromApi().then(setGameInfo).catch(console.error);
-  }, []);
+  useEffect(() => { refreshGameState().catch(console.error); }, []);
 
-  const handleTileClick = (i: number, j: number, player = 1) => {
-    submitMove(player, i, j)
-    .then(() => Promise.all([
-      loadCurrentBoard().then(setTiles),
-      getGameInfoFromApi().then(setGameInfo)
-    ]))
-    .catch(console.error);
+  const handleTileClick = (i: number, j: number) => {
+    submitMove(gameInfo.TurnPlayer, i, j)
+      .then(refreshGameState)
+      .catch(console.error);
+  };
 
-    submitMove(player * -1, i, j)
-    .then(() => Promise.all([
-      loadCurrentBoard().then(setTiles),
-      getGameInfoFromApi().then(setGameInfo)
-    ]))
-    .catch(console.error);
+  const handleResetClick = () => {
+    startNewGame()
+      .then(refreshGameState)
+      .catch(console.error);
   };
   
   return (
     <>
       <NavBar />
       <main className="container">
-        <GameInterface tiles={tiles} gameInfo={gameInfo} onTileClick={handleTileClick}/>
+        <GameInterface tiles={tiles} gameInfo={gameInfo} onTileClick={handleTileClick} onReset={handleResetClick}/>
       </main>
     </>
   );
