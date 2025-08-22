51500f35aa65b2ede47d4fcef2b9c1d6bbd425b8
eeoooue <pdmoorhouse@gmail.com>
2025-08-22 17:31:39 +0100

    GameSidePanel component accepts gameInfo prop and presents values

--- GameplaySPA/src/components/GameSidePanel.tsx
+++ GameplaySPA/src/components/GameSidePanel.tsx
@@ -1,27 +1,44 @@
 
 import './GameSidePanel.css'
+import type { GameInfo } from "../services/GameInfoService";
 
-export default function GameSidePanel() {
+function ParseTurnPlayer(player: number): string {
+  if (player == 1){
+    return "Black";
+  }
+  return "White";
+}
+
+export default function GameSidePanel({
+  gameInfo,
+}: {
+  gameInfo: GameInfo;
+}) {
+
+  const blackScore: number = gameInfo.ScoreBlack;
+  const whiteScore: number = gameInfo.ScoreWhite;
+  const turnPlayer: string = ParseTurnPlayer(gameInfo.TurnPlayer);
+  const enableButton: boolean = gameInfo.PlayerMustPass;
 
   return (
     <div className="game-side-panel">
 
         <div className="game-score">
             <div className="score-panel black">
-              <h3>10</h3>
+              <h3>{blackScore}</h3>
             </div>
             <div className="spacer"></div>
             <div className="score-panel">
-              <h3>10</h3>
+              <h3>{whiteScore}</h3>
             </div>
         </div>
 
         <div className="game-status">
-          <h4>White's Turn</h4>
+          <h4>{turnPlayer}'s Turn</h4>
         </div>
 
         <div className="action-buttons">
-          <button disabled={true}>Pass</button>
+          <button disabled={!enableButton}>Pass</button>
           <button>Concede</button>
         </div>
 
