b0ec1bd32f5db3c458d03986214c5f5327cefa4b
eeoooue <pdmoorhouse@gmail.com>
2025-08-23 18:23:40 +0100

    added CORS

--- OpponentAPI/app.py
+++ OpponentAPI/app.py
@@ -1,8 +1,10 @@
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from othello_board_analyst import OthelloBoardAnalyst
 
 app = Flask(__name__)
+CORS(app)
 
 analyst = OthelloBoardAnalyst()
 
